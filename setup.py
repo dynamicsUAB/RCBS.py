5a164e6116cb7aeefdeac2a17116b8002d721e7a
Miki <miqeelcn@gmail.com>
2020-07-20 19:00:03 +0200

    Modified the version number (now it is the 0.0a0) in order to match the installer specifications

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='RCBS.py',
-        version='0.0a',
+        version='0.0a0',
         packages= find_packages(),
         include_package_data=True)
 

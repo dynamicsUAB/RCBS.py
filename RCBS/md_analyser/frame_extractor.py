8568d4c185b99e499cdfe611d2903ab77cef70e9
Miki <miqeelcn@gmail.com>
2021-04-12 18:32:06 +0200

    Minor update for fixing

--- RCBS/md_analyser/frame_extractor.py
+++ RCBS/md_analyser/frame_extractor.py
@@ -12,12 +12,11 @@ def trajectory_frame_extractor(u, frame, name='', folder='', outformat='.pdb', s
                 --> It has to be a trajectory.
         frame   --> it can be a single frame (input as number or string of number) or a list of frames.
         name    --> name for the output file. If more than one frame is input, outputs will be secuencially named
-                --> by default, output will be saved as pdb. All formats accepted by the write module of MDAnalysis are accepted.
         folder  --> folder name where pdbs have to be saved.
                 --> if the folder does not exist, it will be created.
                 --> by default, no folder is used
         format  --> format for the output file
-                --> by default, the format is pdb
+                --> by default, output will be saved as pdb. All formats accepted by the write module of MDAnalysis are accepted.
 
     """
 
@@ -26,17 +25,16 @@ def trajectory_frame_extractor(u, frame, name='', folder='', outformat='.pdb', s
     if folder != '':
         check_folder(folder)
 
-    if solvent == True:
-        sel = u.select_atoms('all')
-    elif solvent == False:
-        sel = u.select_atoms('all and not (resname WAT or resname NA+ or resname Na+ or resname Cl- or resname CL-)')
 
     if isinstance(frame, list):
         f_ = 0
         for f in frame:
             f_ += 1
             u.trajectory[int(f)]
-            sel = u.select_atoms('all')
+            if solvent == True:
+                sel = u.select_atoms('all')
+            elif solvent == False:
+                sel = u.select_atoms('all and not (resname WAT or resname NA+ or resname Na+ or resname Cl- or resname CL-)')
 
             if name == '':
                 file_name = folder + '/' + str(f)
@@ -47,13 +45,17 @@ def trajectory_frame_extractor(u, frame, name='', folder='', outformat='.pdb', s
 
     if isinstance(frame, (str, int)):
         u.trajectory[int(frame)]
+        if solvent == True:
+            sel = u.select_atoms('all')
+        elif solvent == False:
+            sel = u.select_atoms('all and not (resname WAT or resname NA+ or resname Na+ or resname Cl- or resname CL-)')
 
         if name == '':
             file_name = folder + str(frame)
         else :
             file_name = folder + name
 
-        structure_saver(sel, file_name, format)
+        structure_saver(sel, file_name, outformat)
 
 
 
